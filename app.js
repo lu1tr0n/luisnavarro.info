fcb91aace1e24a6c48267c54491afe1ab59c5a93
Luis Navarro <lu1tr0n.gh0st@gmail.com>
2017-07-10 14:27:15 -0600

    Site CV Personal - Luis Navarro

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+//   _                                            _        __       
+//  | |_   _(_)___ _ __   __ ___   ____ _ _ __ _ __ ___  (_)_ __  / _| ___  
+//  | | | | | / __| '_ \ / _` \ \ / / _` | '__| '__/ _ \ | | '_ \| |_ / _ \ 
+//  | | |_| | \__ \ | | | (_| |\ V / (_| | |  | | | (_) || | | | |  _| (_) |
+//  |_|\__,_|_|___/_| |_|\__,_| \_/ \__,_|_|  |_|  \___(_)_|_| |_|_|  \___/ 
+/*
+ *  Programador: Luis Navarro(lu1tr0n)
+ *  Sitio: luisnavarro.info
+ *  Descripcion: Sitio personal de información academica
+ */
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
